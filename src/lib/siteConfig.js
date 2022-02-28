28291d885867e5da52bbd367859dea637b0b1e4b
René Kuhn <55456939+RocketRene@users.noreply.github.com>
2022-02-28 02:56:21 +0100

    change config

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -1,11 +1,11 @@
 export const SITE_URL = 'https://swyxkit.netlify.app';
-export const GH_USER_REPO = 'sw-yx/swyxkit'; // used for pulling github issues and offering comments
+export const GH_USER_REPO = 'RocketRene/swyxkit-netlify'; // used for pulling github issues and offering comments
 export const REPO_URL = 'https://github.com/' + GH_USER_REPO;
-export const SITE_TITLE = 'SwyxKit';
-export const SITE_DESCRIPTION = "swyx's default SvelteKit + Tailwind starter";
+export const SITE_TITLE = 'René Kuhn';
+export const SITE_DESCRIPTION = "Personal Blog";
 export const DEFAULT_OG_IMAGE =
 	'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';
-export const MY_TWITTER_HANDLE = 'swyx';
+export const MY_TWITTER_HANDLE = 'rocketrene1';
 export const MY_YOUTUBE = 'https://youtube.com/swyxTV';
 
 // dont forget process.env.GH_TOKEN
